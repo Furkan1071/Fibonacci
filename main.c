32e413a2602f4e067c02812cdd1f06ec38a9c413
Furkan <123004647+Furkan1071@users.noreply.github.com>
2023-04-02 20:26:06 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int f=1,i=1,s;
+    printf("%d",f);
+    for (s=0; s<12; s++)
+    {
+        printf("\n%d",f);
+        i+=f;
+        printf("\n%d",i);
+        f+=i;
+    }
+    return 0;
+}
